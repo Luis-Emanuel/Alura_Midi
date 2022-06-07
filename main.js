31342168806657a2501f35c100f527bf7fa0271d
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-06-07 16:36:15 -0300

    Colocando som nas primeiras teclas

---
+++ main.js
@@ -0,0 +1,11 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while(contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom
+    contador ++;
+}
